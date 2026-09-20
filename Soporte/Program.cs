7cdd5816e405060116d584f45bc4a2c045c7d5c7
Lean <N00502100@upn.pe>
2026-09-20 18:45:52 -0500

    captura de datos

--- Soporte/Program.cs
+++ Soporte/Program.cs
@@ -1,8 +1,22 @@
 ﻿using System;
+using System.Net.WebSockets;
 class Program
 {
     static void Main()
     {
         System.Console.WriteLine("Sistema de soporte academico");
+        System.Console.WriteLine("CODIGO: ");
+        string codigo = Console.ReadLine();
+
+        System.Console.WriteLine("Nombre: ");
+        string nombre = Console.ReadLine();
+
+        System.Console.WriteLine("Tipo: ");
+        string tipo = Console.ReadLine();
+
+        System.Console.WriteLine("Descripción: ");
+        string descripcion = Console.ReadLine();
+
+        System.Console.WriteLine("Datos recibidos");
     }
 }
